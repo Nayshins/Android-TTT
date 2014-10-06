6fafbc569b99baaddfc8ac0e0f54f72361b55c35
Jake Nations <jnations@8thlight.com>
2014-10-06 12:36:59 -0500

    Add monkey UI test to gradle and Travis.yml

--- app/src/androidTest/java/jakenations/me/tictactoe/VsComputerTest.java
+++ app/src/androidTest/java/jakenations/me/tictactoe/VsComputerTest.java
@@ -7,9 +7,6 @@ import android.test.suitebuilder.annotation.MediumTest;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Created by Nayshins on 10/2/14.
- */
 public class VsComputerTest extends ActivityInstrumentationTestCase2 {
     private VsComputer vsComputer;
 
@@ -57,5 +54,17 @@ public class VsComputerTest extends ActivityInstrumentationTestCase2 {
         final View view = vsComputer.getWindow().getDecorView();
         Button cell = (Button) view.findViewById(R.id.c1);
         assertFalse(cell.isEnabled());
+    }
+
+    @MediumTest
+    public void testGameOver() {
+        final View view = vsComputer.getWindow().getDecorView();
+        for (int i = 0; i < 9 ; i++) {
+            if (i % 2 == 0 || i == 0) {
+                Button cell = (Button) view.findViewWithTag("c" + i);
+                TouchUtils.clickView(this, cell);
+            }
+        }
+        assertTrue(vsComputer.rules.isGameOver());
     }
 }
--- app/src/androidTest/java/jakenations/me/tictactoe/VsHumanTest.java
+++ app/src/androidTest/java/jakenations/me/tictactoe/VsHumanTest.java
@@ -7,9 +7,7 @@ import android.test.suitebuilder.annotation.MediumTest;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Created by Nayshins on 10/2/14.
- */
+
 public class VsHumanTest extends ActivityInstrumentationTestCase2 {
         private VsHuman vsHuman;
 
@@ -72,5 +70,15 @@ public class VsHumanTest extends ActivityInstrumentationTestCase2 {
             final View view = vsHuman.getWindow().getDecorView();
             Button cell = (Button) view.findViewById(R.id.c1);
             assertFalse(cell.isEnabled());
+        }
+
+        @MediumTest
+        public void testGameOver() {
+            final View view = vsHuman.getWindow().getDecorView();
+            for (int i = 0; i < 9 ; i++) {
+                Button cell = (Button) view.findViewWithTag("c" + i);
+                TouchUtils.clickView(this, cell);
+            }
+            assertTrue(vsHuman.rules.isGameOver());
         }
 }
